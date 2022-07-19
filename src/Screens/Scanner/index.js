f771843d48ce943c3c874510386970cf2d96ec6e
santhosh <santhoshkumar@omneky.com>
2022-07-20 03:10:48 +0530

    qr

---
+++ src/Screens/Scanner/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Col, Container, Form, Image, Row } from 'react-bootstrap'; 
+import Header from '../../Components/Header';
+import { QrReader } from 'react-qr-reader';
+const Scanner = () => { 
+    const [Data, setData] = useState('No result');
+    const handleScan = data => {
+        if (data) {
+             setData(data)
+        }
+    }
+    const handleError = err => {
+    console.error(err)
+    }
+  return (
+    <Container fluid className='main-page'>
+        <Header />
+      <Container>
+      <div style={{marginTop:30}}>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+            </div>
+            <p>{Data}</p>
+      </Container>
+    </Container>
+  );
+}
+
+export default Scanner;
